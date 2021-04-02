2e2e403f8e0cb8a1ac443d36c282dbfd562b03eb
axutoz19 <axutoz_faith@yahoo.com>
2021-04-02 23:57:17 +0545

    Final TODO app completed

---
+++ app/src/main/java/com/aa/todoapp/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.aa.todoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        //creating the object for the thread
+        Thread thread =new Thread(){
+
+            public void run(){
+                try {
+                    sleep(4500);
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+
+                    Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };thread.start();
+
+    }
+}
